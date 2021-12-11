1dcb5fc17352c45055f048ab234b0541e35fd7c6
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-12 01:41:14 +0300

    control syn

--- control_syn.py
+++ control_syn.py
@@ -5,7 +5,7 @@ from myhdl import *
 # from DataMemory import copy_Mem1,copy_Mem2,copy_Mem3,copy_Mem4
 
 @block
-def control(opcode, func3, func7, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
+def control_syn(opcode, func3, func7, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
             rs2_or_imm_or_4, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg, sign_selection):
     @always_comb
     def control_cir():
@@ -287,16 +287,59 @@ def control(opcode, func3, func7, size_sel, operation_sel, enable_write, PC_genr
     return instances()
 
 
-class copy:
-    def __init__(self):
-        self.copy_register = []
-        self.Mem1 = []
-        self.Mem2 = []
-        self.Mem3 = []
-        self.Mem4 = []
+@block
+def test_bench():
+    opcode = Signal(intbv(0)[7:])
+    func3 = Signal(intbv(0)[3:])
+    func7 = Signal(intbv(0)[7:])
+    size_sel = Signal(intbv(0)[2:])
+    operation_sel = Signal(intbv(0)[5:])
+    enable_write = Signal(bool(0))
+    PC_genrator_sel = Signal(intbv(0)[2:])
+    imm_sel = Signal(intbv(0)[3:])
+    rs2_or_imm_or_4 = Signal(intbv(0)[2:])
+    PC_or_rs1 = Signal(bool(0))
+    ALU_or_load_or_immShiftedBy12 = Signal(intbv(0)[2:])
+    Shift_amount = Signal(intbv(0)[2:])
+    Enable_Reg = Signal(bool(0))
+    sign_selection = Signal(intbv(0)[2:])
+    ins = control_syn(opcode, func3, func7, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
+                  rs2_or_imm_or_4, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg, sign_selection)
 
+    @instance
+    def stimulus():
+        opcode.next, func3.next, func7.next = 0b0110011, 0b000, 0b0000000
+        yield delay(1)
+        print("-" * 30)
+        print("| Opcode: %s            |" % bin(opcode.next, 7))
+        print("| func3: %-20s|" % (func3.next + 0))
+        print("| func7: %-20s|" % (func7.next + 0))
+        print("-" * 30)
+        print("|===================================|")
+        print("| size_sel                      | %-1s |" % (size_sel.next + 0))
+        print("|-------------------------------+---|")
+        print("| operation_sel                 | %-1s |" % (operation_sel.next + 0))
+        print("|-------------------------------+---|")
+        print("| enable_write                  | %-1s |" % (enable_write.next + 0))
+        print("|-------------------------------+---|")
+        print("| PC_genrator_sel               | %-1s |" % (PC_genrator_sel.next + 0))
+        print("|-------------------------------+---|")
+        print("| imm_sel                       | %-1s |" % (imm_sel.next + 0))
+        print("|-------------------------------+---|")
+        print("| rs2_or_imm_or_4               | %-1s |" % (rs2_or_imm_or_4.next + 0))
+        print("|-------------------------------+---|")
+        print("| PC_or_rs1                     | %-1s |" % (PC_or_rs1.next + 0))
+        print("|-------------------------------+---|")
+        print("| ALU_or_load_or_immShiftedBy12 | %s |" % (ALU_or_load_or_immShiftedBy12.next + 0))
+        print("|-------------------------------+---|")
+        print("| Shift_amount                  | %-1s |" % (Shift_amount.next + 0))
+        print("|-------------------------------+---|")
+        print("| Enable_Reg                    | %-1s |" % (Enable_Reg.next + 0))
+        print("|-------------------------------+---|")
+        print("| sign_selection                | %-1s |" % (sign_selection.next + 0))
+        print("|===================================|")
 
-obj = copy()
+    return instances()
 
 
 def convert():
@@ -314,8 +357,11 @@ def convert():
     Shift_amount = Signal(intbv(0)[2:])
     Enable_Reg = Signal(bool(0))
     sign_selection = Signal(intbv(0)[2:])
-    ins = control(opcode, func3, func7, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
-                  rs2_or_imm_or_4, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg, sign_selection)
+    ins = control_syn(opcode, func3, func7, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
+            rs2_or_imm_or_4, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg, sign_selection)
     ins.convert(hdl='Verilog')
 
-convert()
+
+# test = test_bench()
+# test.run_sim()
+# convert()
