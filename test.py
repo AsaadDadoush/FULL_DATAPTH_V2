8651a67de1b55eda727a5d3e6d7e4c6266cf85c8
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-14 23:28:54 +0300

    test

--- test.py
+++ test.py
@@ -4,20 +4,17 @@ def DataMemory(data_in, enable, size, address, data_out, clk, load_data, load_ad
     Mem2 = [Signal(intbv(0)[8:]) for i in range(3072)]
     Mem3 = [Signal(intbv(0)[8:]) for i in range(3072)]
     Mem4 = [Signal(intbv(0)[8:]) for i in range(3072)]
-
     @always(clk.posedge)
     def load_logic():
         Mem1[load_address].next = load_data[8:]
         Mem2[load_address].next = load_data[16:8]
         Mem3[load_address].next = load_data[24:16]
         Mem4[load_address].next = load_data[32:24]
-
     @always_seq(clk.posedge, reset=None)
     def Write_logic():
         if enable == 1:
             if size == 0:
                 Mem1[address].next = data_in[8:0]
-
             elif size == 1:
                 Mem1[address].next = data_in[8:0]
                 Mem2[address].next = data_in[16:8]
@@ -35,5 +32,4 @@ def DataMemory(data_in, enable, size, address, data_out, clk, load_data, load_ad
         else:
             data_out.next = concat(Mem4[address], Mem3[address], Mem2[address],
                                    Mem1[address])
-
     return instances()
