ed30931b3c9947aacc583f54a400c702fe789ac3
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-14 23:43:23 +0300

    test

--- test.py
+++ test.py
@@ -1,35 +1,23 @@
 @block
-def DataMemory(data_in, enable, size, address, data_out, clk, load_data, load_address):
-    Mem1 = [Signal(intbv(0)[8:]) for i in range(3072)]
-    Mem2 = [Signal(intbv(0)[8:]) for i in range(3072)]
-    Mem3 = [Signal(intbv(0)[8:]) for i in range(3072)]
-    Mem4 = [Signal(intbv(0)[8:]) for i in range(3072)]
-    @always(clk.posedge)
-    def load_logic():
-        Mem1[load_address].next = load_data[8:]
-        Mem2[load_address].next = load_data[16:8]
-        Mem3[load_address].next = load_data[24:16]
-        Mem4[load_address].next = load_data[32:24]
-    @always_seq(clk.posedge, reset=None)
-    def Write_logic():
-        if enable == 1:
-            if size == 0:
-                Mem1[address].next = data_in[8:0]
-            elif size == 1:
-                Mem1[address].next = data_in[8:0]
-                Mem2[address].next = data_in[16:8]
-            else:
-                Mem1[address].next = data_in[8:0]
-                Mem2[address].next = data_in[16:8]
-                Mem3[address].next = data_in[24:16]
-                Mem4[address].next = data_in[32:24]
+def registers(rs1, rs2, rd, rs1_out, rs2_out, enable, DataWrite, clk):
+    Reg = [Signal(intbv(0, min=-2 ** 31, max=2 ** 31)) for i in range(32)]
+    Reg[0] = Signal(intbv(0)[32:])
+    Reg[0]._update()
+    Reg[2] = Signal(intbv(10016)[32:])
+    Reg[2]._update()
+    Reg[3] = Signal(intbv(6144)[32:])
+    Reg[3]._update()
+
     @always_comb
     def Read_logic():
-        if size == 0:
-            data_out.next = concat("00000000", "00000000", "00000000", Mem1[address])
-        elif size == 1:
-            data_out.next = concat("00000000", "00000000", Mem2[address], Mem1[address])
-        else:
-            data_out.next = concat(Mem4[address], Mem3[address], Mem2[address],
-                                   Mem1[address])
+        rs1_out.next = Reg[rs1].signed()
+        rs2_out.next = Reg[rs2].signed()
+    @always_seq(clk.posedge, reset=None)
+    def write_logic():
+        if enable == 1:
+            Reg[rd].next = DataWrite
+            Reg[rd]._update()
+            Reg[0].next = 0
+            Reg[0]._update()
+
     return instances()
